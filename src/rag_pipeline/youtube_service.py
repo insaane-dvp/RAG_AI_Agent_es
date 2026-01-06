29b6c431dbd0d46e42acb451fd9277eb78f25295
insaane-dvp <tgio.dvp@gmail.com>
2026-01-06 12:00:42 +0100

    continuo Youtube Service con Supadata

--- src/rag_pipeline/youtube_service.py
+++ src/rag_pipeline/youtube_service.py
@@ -16,7 +16,7 @@ class TranscriptSegment(BaseModel):
 class YoutubeService:
     """Servizio per utilizzare l'API di YouTube v3"""
 
-    def __init__(self, api_key: str, ID_channel: str):
+    def __init__(self, api_key: str, ID_channel: str) -> None:
         """Inizializzazione del Servizio di Youtube.
         
         Args:
@@ -29,7 +29,6 @@ class YoutubeService:
         
         self.api_key = api_key
         self.ID_channel = ID_channel
-        self.base_url = "https://www.googleapis.com/youtube/v3"
 
         #Validazione del formato dell'ID del canale
 
@@ -37,3 +36,34 @@ class YoutubeService:
             raise ValueError(
                 f"Invalid ID_channel: {ID_channel}. It must start with 'UC' and be 24 chars long"
             )
+        if int(os.getenv("YOUTUBE_LOOKBACK_DAYS", "7")) < 1:
+            raise ValueError(
+                "Lookback days must be greater than 0"
+                )
+        
+        self.base_url = "https://www.googleapis.com/youtube/v3"
+        
+        async def search_videos(self) -> list[Video]:
+            """
+            Cerca video nel canale configurato pubblicati negli ultimi X giorni.
+        
+            I video vengono restituiti in ordine di data di pubblicazione, dal più recente al meno recente
+
+            Returns: 
+                Lista di oggetti Video che contengono i metadata del video
+            """
+            published_after = (
+                datetime.now(timezone.utc) - timedelta(days = int(os.getenv("YOUTUBE_LOOKBACK_DAYS", "7")))
+            ).isoformat().replace("+00:00", "Z")
+
+            params = {
+                "part": "snippet_id",
+                "channelId": self.ID_channel,
+                "order": "date",
+                "type": "video",
+                "eventType": "completed",
+                "publishedAfter": published_after,
+                "maxResults": 50,
+                "key": self.api_key,
+            }
+
